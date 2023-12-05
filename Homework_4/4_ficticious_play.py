b82263ee56a0217714a9172607f1b810f69d5354
Julian <julpo99@icloud.com>
2023-12-05 23:50:39 +0100

    Section 4 modified (as well as 4_ficticious_play.py)

--- Homework_4/4_ficticious_play.py
+++ Homework_4/4_ficticious_play.py
@@ -13,7 +13,7 @@ player_2_pi_matrix = np.array([player_2_counts[0] / np.sum(player_2_counts), pla
     player_2_counts), player_2_counts[2] / np.sum(player_2_counts), player_2_counts[3] / np.sum(player_2_counts)])
 
 
-for i in range(1000000):
+for i in range(100):
     player_1_payoff = np.dot(payoff_matrix_player_1, player_2_pi_matrix)
     player_2_payoff = np.dot(payoff_matrix_player_2, player_1_pi_matrix)
 
@@ -30,8 +30,14 @@ print("Player 1 pi matrix: ", player_1_pi_matrix)
 print("Player 2 pi matrix: ", player_2_pi_matrix)
 
 
-# Results
+# Results 1000000 iterations
 # Player 1 counts:  [     1 500556 499446]
 # Player 2 counts:  [     1 600020 399982      1]
 # Player 1 pi matrix:  [9.99997000e-07 5.00554498e-01 4.99444502e-01]
 # Player 2 pi matrix:  [9.999960e-07 6.000176e-01 3.999804e-01 9.999960e-07]
+
+# Results 100 iterations
+# Player 1 counts:  [ 1 47 55]
+# Player 2 counts:  [ 1 64 38  1]
+# Player 1 pi matrix:  [0.00970874 0.45631068 0.53398058]
+# Player 2 pi matrix:  [0.00961538 0.61538462 0.36538462 0.00961538]
